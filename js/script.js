24d522b5c5a20730b7dc3d352b479a1fdc869b90
virtu-alex <alessio.cordari@gmail.com>
2022-07-06 16:54:05 +0200

    milestone 2.5

--- js/script.js
+++ js/script.js
@@ -18,7 +18,7 @@ Ogni post dovrà avere le informazioni necessarie per stampare la relativa card:
 
 
 
-#Milestone 2
+#////Milestone 2
 Prendendo come riferimento il layout di esempio presente nell'html, stampiamo i post del nostro feed.
 
 //creo un ciclo for per stampare in pagina
@@ -35,42 +35,106 @@ Se clicchiamo sul tasto "Mi Piace" cambiamo il colore al testo del bottone e inc
 //RECUPERO ELEMENTO CONTAINER DAL DOM
 const container = document.getElementById('container')
 //RECUPERO BOTTONE LIKES
-const button = document.getElementById('like-counter-1')
 
 
 const post = [
     {
         id: 1,
         username: 'Alessio Cordari',
-        userPic: 'alessio.jpg',
+        userPic: 'https://unsplash.it/300/300?image=110',
         date: '09/14/2022',
         text: 'Distinctio minima iusto',
-        pic: 'https://unsplash.it/300/300?image=108',
-        likes: '100'
+        pic: 'https://unsplash.it/600/300?image=108',
+        likes: 184
     },
     {
         id: 2,
         username: 'Marco Lanci',
-        userPic: 'marco.jpg',
+        userPic: 'https://unsplash.it/300/300?image=109',
         date: '06/10/2020',
         text: 'Placeat libero ipsa nobis',
-        pic: 'https://unsplash.it/300/300?image=215',
-        likes: '120'
+        pic: 'https://unsplash.it/600/300?image=215',
+        likes: 125
     },
     {
         id: 3,
         username: 'Simone Sanna',
-        userPic: 'simone.jpg',
+        userPic: 'https://unsplash.it/300/300?image=102',
         date: '01/05/2021',
         text: 'Et nihil ullam aut alias',
-        pic: 'https://unsplash.it/300/300?image=300',
-        likes: '200'
+        pic: 'https://unsplash.it/600/300?image=305',
+        likes: 213
+    },
+    {
+        id: 4,
+        username: 'Fabrizio Mastrobattista',
+        userPic: 'https://unsplash.it/300/300?image=133',
+        date: '01/05/2021',
+        text: 'Et nihil ullam aut alias',
+        pic: 'https://unsplash.it/600/300?image=315',
+        likes: 2000
+    },
+    {
+        id: 5,
+        username: 'Enrico Campani',
+        userPic: 'https://unsplash.it/300/300?image=122',
+        date: '01/05/2021',
+        text: 'Et nihil ullam aut alias',
+        pic: 'https://unsplash.it/600/300?image=325',
+        likes: 500
     },
 ]
+
+console.table(post)
 
 //CICLO FOR SULL'ARRAY
 for (let i = 0; i < post.length; i++) {
     //VARIABILE PER EVITARE RIPETIZIONI NEL CODICE
     let current = post[i];
+    container.innerHTML += `<div class="post">
+    <div class="post__header">
+    <div class="post-meta">
+    <div class="post-meta__icon">
+    <img class="profile-pic" src="${current.userPic}" alt="${current.username}" />
+    </div>
+    <div class="post-meta__data">
+    <div class="post-meta__author">${current.username}</div>
+    <div class="post-meta__time">${current.date}</div>
+    </div>
+    </div>
+    </div>
+    <div class="post__text">
+    ${current.text}
+    </div>
+    <div class="post__image">
+    <img src="${current.pic}" alt="${current.username}" />
+    </div>
+    <div class="post__footer">
+    <div class="likes js-likes">
+    <div class="likes__cta">
+    <a class="like-button js-like-button" href="#" data-postid="${current.id}">
+    <i class="like-button__icon fas fa-thumbs-up" aria-hidden="true"></i>
+    <span class="like-button__label">Mi Piace</span>
+    </a>
+    </div>
+    <div class="likes__counter">Piace a <b id="like-counter-1" class="js-likes-counter">${current.likes}</b> persone</div>
+    </div>
+    </div>
+    </div>
+    </div>`
+    
+};
 
-}
+
+const button = document.querySelector('.like-button')
+
+
+button.addEventListener('click', function() {
+     button.classList.toggle('like-button--liked')
+     if(button.contains('like-button--liked')){
+        `${current.likes + 1}`
+     }
+    })
+
+
+    
